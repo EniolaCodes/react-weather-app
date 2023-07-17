1a9c8e427f4b452425aad15cf4b424d2ca7a5946
EniolaCodes <adebimpeniola@gmail.com>
2023-07-17 14:44:38 +0100

    added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">Weather App </div>  
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by {""}
+        <a
+          href="https://www.linkedin.com/in/fatimah-adebimpe-441b10145"
+          target="_blank"
+        >
+          Fatimah Adebimpe
+        </a>{" "}
+        and is {""}
+        <a
+          href="https://github.com/EniolaCodes/react-weather-app.git"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
   );
-  }
+}
