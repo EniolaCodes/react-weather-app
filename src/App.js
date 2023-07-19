a1ba73802e8252a300c69179ca2d5da45c1d24f8
EniolaCodes <adebimpeniola@gmail.com>
2023-07-19 18:12:12 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by {""}
-        <a
-          href="https://www.linkedin.com/in/fatimah-adebimpe-441b10145"
-          target="_blank"
-        >
-          Fatimah Adebimpe
-        </a>{" "}
-        and is {""}
-        <a
-          href="https://github.com/EniolaCodes/react-weather-app.git"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by {""}
+          <a
+            href="https://www.linkedin.com/in/fatimah-adebimpe-441b10145"
+            target="_blank"
+          >
+            Fatimah Adebimpe
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/EniolaCodes/react-weather-app.git"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
