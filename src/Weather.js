1496ad02793c436f516a7eaf6826c066a860cb93
EniolaCodes <adebimpeniola@gmail.com>
2023-07-22 16:22:02 +0100

    added the date in the Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
+import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
+    console.log(response.data);
     setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
       city: response.data.name,
       wind: response.data.wind.speed,
       description: response.data.weather[0].description,
-      date: "Wednesday 07:00",
       iconUrl: `https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png`,
     });
   }
@@ -40,7 +43,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row">
@@ -70,7 +75,7 @@ export default function Weather(props) {
     );
   } else {
     const apiKey = "57b2c40fdae71a6ba41d72685e3226e2";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
     return "Loading...";
   }
