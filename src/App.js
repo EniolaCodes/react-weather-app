fdda0f72004caf62ccfc575b2fea131d650f9062
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-05-23 13:46:48 +0100

    change the background and text color

--- src/App.js
+++ src/App.js
@@ -4,29 +4,11 @@ import Weather from "./Weather";
 import "./Weather.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <Weather defaultCity="Lagos" />
-        <footer>
-          This project was coded by {""}
-          <a
-            href="https://www.linkedin.com/in/fatimah-adebimpe-441b10145"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Fatimah Adebimpe
-          </a>{" "}
-          and is {""}
-          <a
-            href="https://github.com/EniolaCodes/react-weather-app.git"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-        </footer>
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				<Weather defaultCity="Lagos" />
+			</div>
+		</div>
+	);
 }
